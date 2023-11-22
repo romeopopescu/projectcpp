51e44937e92a96d93a0cb9e7c9dee3ede7e6a2f1
Romeo Popescu <popescuromeo22@stud.ase.ro>
2023-11-22 21:40:17 +0200

    Written a part of the eventLocation class.

--- myProject/Source.cpp
+++ myProject/Source.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 
-class ticket {
+using namespace std;
 
+class eventLocation {
+	int maxSeats;
+	int rows;
+	int zones;
+	int seatNumber;
+
+	eventLocation() {
+		this->maxSeats = 10000;
+		this->rows = 100;
+		this->zones = 5;
+		this->seatNumber = 2;
+	}
+
+public:
+
+	void setMaxSeats(int nrSeats) {
+		if (nrSeats < 0) {
+			throw exception("Negative number of seats");
+		}
+		this->maxSeats = nrSeats;
+	}
 };
 
 int main() {
