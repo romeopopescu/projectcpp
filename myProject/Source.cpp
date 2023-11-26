5afb58e4c80d4539bbd4a3a5df41fa1fce25e2ce
Romeo Popescu <popescuromeo22@stud.ase.ro>
2023-11-26 10:23:03 +0200

    Getters and setters for EventLocation and EventDetails classes

--- myProject/Source.cpp
+++ myProject/Source.cpp
@@ -2,44 +2,120 @@
 
 using namespace std;
 
-class eventLocation {
+class EventLocation {
 	int maxSeats;
-	int rows;
 	int zones;
-	int seatNumber;
+	string name;
 	
 
 public:
 
-	eventLocation() {
-		maxSeats = 10000;
-		rows = 100;
-		zones = 5;
-		seatNumber = 2;
+	//Default constructor
+	EventLocation(int maxSeats1, int zones1, const string nameLoc) {
+		this->setMaxSeats(maxSeats1);
+		this->setZones(zones1);
+		this->setName(nameLoc);
 	}
 
+	//Setters
 	void setMaxSeats(int nrSeats) {
 		if (nrSeats < 0) {
 			throw exception("Negative number of seats");
 		}
-		maxSeats = nrSeats;
+		this->maxSeats = nrSeats;
 	}
 
-	int getMaxSeats() {
-		return maxSeats;
+	void setZones(int nrZones) {
+		if (nrZones < 0) {
+			throw exception("Negative number of rows");
+		}
+		this->zones = nrZones;
 	}
+	
+	void setName(const string nameLoc) {
+		if (nameLoc.length() < 3) {
+			throw exception("Location too short");
+		}
+		this->name = nameLoc;
+	}
+
+	//Getters
+	int getMaxSeats() {
+		return this->maxSeats;
+	}
+
+	int getZones() {
+		return this->zones;
+	}
+
+	string getName() {
+		return this->name;
+	}
+
+	
 
 
 
 
 };
 
-class eventDetails {
-	int date;
-	int time;
-	char* name;
+class EventDetails {
+	string date;
+	string time;
+	string eventType;
 
 public:
+
+	//Default constructor
+	EventDetails(string date, string time, string eventType) {
+		this->setDate(date);
+		this->setTime(time);
+		this->setEventType(eventType);
+	}
+
+
+	//Setters
+	void setDate(string date1) {
+		if (date1.length() != 10) {
+			throw exception("Date format: dd/mm/yyyy");
+		}
+		this->date = date1;
+	}
+
+	void setTime(string time1) {
+		if (time1.length() != 5) {
+			throw exception("Time format: hh:mm, ex: 18:15");
+		}
+		this->time = time1;
+	}
+
+	void setEventType(const string type) {
+		
+		string type1 = type;
+		
+		for (int i = 0; i < type.length(); i++) {
+			type1[i] = toupper(type[i]);
+		}
+		
+		if (type1 != "MOVIE" && type1 != "FOOTBALL MATCH") {
+			throw exception("Available event types: movie and football match");
+		}
+		this->eventType = type;
+	}
+
+	//Getters
+	string getDate() {
+		return this->date;
+	}
+
+	string getTime() {
+		return this->time;
+	}
+
+	string getType() {
+		return this->eventType;
+	}
+
 
 };
 
@@ -54,10 +130,13 @@ public:
 
 int main() {
 	
-	eventLocation arenaNationala;
+	EventLocation fotbal(100, 3, "Arena Nationala");
 
-	arenaNationala.setMaxSeats(-100);
+	cout << fotbal.getName();
 
-	cout << arenaNationala.getMaxSeats();
+	EventDetails fotbalDetails("10/11/2001", "18:00", "Football Match");
+
+	cout << endl << fotbalDetails.getType();
+	
 
 }
